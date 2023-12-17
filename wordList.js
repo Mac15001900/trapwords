30d4997a1d86d4a6accd887d1245377b8d1bf9b5
Mac15001900 <mac15001900@gmail.com>
2023-12-17 01:34:42 +0100

    Added word list from zgadywator and Decrypto

---
+++ wordList.js
@@ -0,0 +1,1120 @@
+basicWords = `Richard Feynman
+Nicola Tesla
+Mikołaj Kopernik
+Erwin Schrödinger
+Max Planck
+Galileusz
+Eugeniusz Rybka
+Maria Skłodowska-Curie
+Mały Obłok Magellana
+Gromada otwarta w Herkulesie
+Katalog Messiera
+Gromada w Pannie
+Droga Mleczna
+Supergromada Lokalna
+Orion
+Ganimedes
+Fobos
+Wężownik
+Krater na Księżycu
+Karpaty
+Tarcza Sobieskiego
+Wielka Czerwona Plama
+Olympus Mons
+Galaktyka Wiatraczek
+Cefeusz
+Projekt OGLE
+Wybuch Betelgezy
+Układ podwójny
+Ciało doskonale czarne
+Widmo emisyjne
+Ciąg
+Jack Sparrow
+Gandalf Szary
+Fiona
+Mulan
+Igrzyska Śmierci
+Zwiadowcy
+Barbie
+Ken
+Oppenheimer
+Doda
+Michael Jackson
+Pałac Kultury
+Kubuś Puchatek
+Kłapouchy
+Adam Małysz
+Mariusz Pudzianowski
+Robert Lewandowski
+Usain Bolt
+Robert Kubica
+Elsa
+Roszpunka
+Tabaluga
+Papa Smerf
+Psi Patrol
+Gumisie
+Pepe Pan Dziobak
+Pan Tadeusz
+Koziołek Matołek
+Bolek I Lolek
+Jurand ze Spychowa
+Jagienka
+Calineczka
+Hermiona
+Severus Snape
+Voldemort
+Król Julian
+Obi-Wan Kenobi
+Uniwersytet Warszawski
+Władca Pierścieni
+Gollum
+Zgredek
+Iron Man
+Neil deGrasse Tyson
+Hulk
+Mario
+Neil Armstrong
+Jackie Chan
+Picasso
+Beethoven
+Rasputin
+Królowa Elżbieta
+Krzesło obrotowe
+Titanic
+Antarktyda
+Mur chiński
+Chińskie bajki
+Catering Werona
+Platon
+Konfucjusz
+Stoicyzm
+Poznań
+Aleje Jerozolimskie
+Kolumna Zygmunta
+Krzywa wieża w Pizie
+Toruńska starówka
+Smok wawelski
+Punkt Barana
+Montaż paralaktyczny
+Montaż azymutalny
+User
+Arbuz
+Ewakuacja
+Małe trąby powietrzne
+Kret
+Mariner
+Krople żołądkowe
+Hamak
+Gumofilce
+Wykryj porównaj
+Mikrofalówka komendanta
+Coś zielonego
+Ciepły asfalt
+Gąszcz
+Udar słoneczny
+Dżdż
+Kapuśniaczek
+Opad konwekcyjny
+Dbz
+Arburza
+Kadra antywychowawcza
+G
+Brudne skarpetki
+Larwa
+Umieralnia
+Entropia
+Spin
+Dynks
+Tenteges
+Tętno
+Tkanka tłuszczowa
+Opornik
+Kosmiczne grzybki
+Kurz
+Układ nerwowy oposa
+Odpowiedzialność zbiorowa
+Motorniczy
+Gotyk
+Mikrosoczewkowanie grawitacyjne
+Grafen
+Mitoza
+Termopauza
+Lawa
+Pole elektromagnetyczne
+Bajt
+Tenor
+Trąba
+Proch strzelniczy
+Okrąg
+Sołtys
+Zbiór liczb całkowitych
+Nic
+Jajko
+Palambury
+Błękit
+Złość
+Juliusz Cezar
+Wielki Wybuch
+Pozytywizm
+Impresjonizm
+Rock psychodeliczny
+Starożytność
+Odkurzacz bezprzewodowy
+AstroNET
+Ekliptyka
+Rektascencja
+Jesteś dzikiem
+Wartowanie
+Linux
+Almukantywka
+Rabini
+Równowaga chwiejna
+Sortowanie bąbelkowe
+Uśmiech bąbelka
+Rozpuszczona czekolada
+Ekwiwalent kiełbasy
+Naleśnik z budyniem
+Gruszka
+Gofr z tofu
+Szpinak
+Eter
+Granit
+Kostka lodu
+Trójkąt
+Następna stacja
+Nieskończoność
+Politechnika Wrocławska
+3 kilogramy
+Rój pszczół
+Cynk
+Materiał wodoodporny
+Piorun
+Rów Mariański
+Próżnia
+Kciuk
+Zmarszczki na czole
+Powietrze
+Szum fal
+Światło
+Karteczka na plecach
+Ściółka na kanadyjce
+Apel
+Obozowa Olimpiada Astronomiczna
+Gwizdek oboźnej
+Czapka Sulika
+Komendant
+Altanka
+Serower
+Serowar
+Serakieta
+Wodąwryj
+Bardzo mała żaba
+Dinozaur Pimpuś
+Parówka sojowa
+Groźna oboźna
+Dziewczyna co piła gorące mleko
+Marszałkowie szklanoocy
+Sarna
+Kanadyjka
+Drechoteka
+Mokry latex
+Złota szyszka
+Plopserwacje
+Niebo żyleta
+Sen srebrny w płynie
+Sen srebrny w stanie stałym
+
+Klub Astrologiczny Akumulator
+Klub Gastronomiczny Almukantratat
+Pierwiastek 0
+Nadwarciański Grób
+Tosty z żelazka
+Zbiór pusty
+Granat zaczepny
+Drzewo sandałowe
+Pajęczy kosz
+Olej rzepakowy
+Redystrybucja butów
+Fregata uzbrojona rzędem w setkę dział
+Złota kula nad wodami
+Para bobrów przytulonych nad potokiem
+Ta keja
+Gwiazda Śmierci
+Człowiek który zgubił do domu swego drzwi
+Wiatr co rozwiewa stopy
+Piersi w panierce
+Przystojny wiatr
+Pieśń skrzydlata
+Zielone karty
+Jaworzyna
+Major Ponury
+Majster Bieda
+Posejdon
+Zeus
+Smutny programista
+Chomik na patelni
+Rybacki kuter
+Morska opowieść
+Całkiem nowa bomba atomowa
+Drzewo cytrynowe
+Diament
+Wehikuł czasu
+Minecraft
+Tetris
+Sudoku
+Kółko i Krzyżyk
+Kamień Papier i Nożyce
+Ktulu
+Zielona macka
+Hazardzista
+Tajniacy
+Pocisk
+Doktorat z Szyszkologii
+Petagram
+Pentagram
+Obrotowa mapka nieba
+Obrotowa mapka Dzietrznik
+Dzietrzniki
+Cisza nocna
+Czwarta nad ranem
+Woda gazowana
+Baniak z wodą
+Mech
+Litosfera
+Ośmiokąt nieforemny
+Kandydat Na Kandydata Na Weterana Obozów Astronomiczncyh
+Piąta ćwiartka
+Międzynarodowa Stacja Kosmiczna
+Tablica ogłoszeń
+Zbieranie szyszek
+Zeszyt w linie
+Matura
+Killer
+Czerwona latarka
+Wielka Całka
+Transformata Fouriera
+Sieć neuronowa
+ChatGPT
+Sztuczna inteligencja
+Sztuczna głupota
+Płyta DVD
+Dyskietka
+Packa na muchy
+Modliszka
+Pszczoła
+Bydło
+Spam
+Flaga Finlandii
+Kraken
+Trebusz
+Mikser
+Obieraczka
+Pikantny sos
+Niezapominajka
+Internet
+Pianka do golenia
+Terraformacja
+Wojna nuklearna
+Hoplita
+Rów Mariański
+Gripex
+Firanka
+Szuflada
+Snopowiązałka
+Zagadka
+Armagedon
+Powódź
+Śrubokręt
+Para wodna
+Rewolucja przemysłowa
+Szczotka do włosów
+Żarówka
+Aorta
+Klawiatura
+Karta graficzna
+Papier ścierny
+Telefon wielokomórkowy
+Wirtualna rzeczywistość
+Święty Mikołaj
+Skórka od banana
+Kalkulator graficzny
+Szwajcarski zegarek
+Rondo
+Rondel
+Karabin snajperski
+
+Almukantarat
+Heliomagnetohydrodynamika
+Śpiewanki
+Gitara
+Załęcze
+Szyszka
+Harcerz
+Latający namiot
+Rozpórka
+Kontrola czystości
+Sanitariat
+Młyn
+Podwieczorek
+Zbiórka
+Pociąg elektryczny
+Kadron
+Wielki Zderzacz Kadronów
+Gra terenowa
+Ognisko
+Obóz
+Śpiwór
+Seminarium
+Zimowisko
+Referat
+Zwycięzca sesji referatowej
+Pojemnik ewakuacyjny
+Skrót myślowy
+
+Śląsk
+Bieszczady
+Paryż
+Teksas
+Hawaje
+Japonia
+Sahara
+Tybet
+Tatry
+Mount Everest
+Antarktyka
+Amazonka
+Google
+Telewizja
+GPS
+Satelita
+Pizza z ananasem
+UFO
+Kremówka
+Bursztyn
+Dzban
+
+Wielbłąd
+Żyrafa
+Krokodyl
+Orzeł
+Wieloryb
+Zółw
+Mamut
+Koliber
+Biedronka
+
+Trawa
+Sekwoja
+Melon
+Jabłonka
+Palma
+
+Przeciwieństwo
+Synonim
+Czas przeszły
+Liczba mnoga
+Łacina
+Polonistyka
+Japonistyka
+Biologia
+Filozofia
+Laboratorium matematyczne
+
+Magnetyzm
+Elektryczność
+Grawitacja
+Energia
+Przewodnik
+Nadprzewodnik
+Opór
+Wymiar
+Pęd
+Układ odniesienia
+Kwark
+Atom
+Superpozycja
+Splątanie
+Niepewność
+Fala
+Promieniowanie
+Pryzmat
+Radar
+Soczewka
+Laser
+Wahadło
+Sprężyna
+Asteroida
+Kometa
+Planeta X
+Księżyc
+Zaćmienie Słońca
+Plama słoneczna
+Koronalny wyrzut masy
+Gwiazda neutronowa
+Czarna dziura
+Tunel czasoprzestrzenny
+Galaktyka
+Wszechświat
+Ciemna materia
+Jasna materia
+Wenus
+Mars
+Pas asteroid
+Perseidy
+Jowisz
+Saturn
+Uran
+Pluton
+Charon
+Obłok Oorta
+Teleskop radiowy
+Winda kosmiczna
+Kot Schrödingera
+
+Magia
+Zaklęcie
+Alchemia
+Mandragora
+Runa
+Iluzja
+Pułapka
+Krasnolud
+Elf
+Syrena
+Driada
+Centaur
+Ork
+Troll
+Skrzat
+Demon
+Smok
+Gryf
+Duch
+Zombie
+Szkielet
+Jednorożec
+Minotaur
+Goblin
+Różdżka
+Latający dywan
+Peleryna niewidka
+Eliksir
+Magiczna kula
+Kocioł
+Czarodziej
+Wiedźma
+Barbarzyńca
+Kleryk
+Złodziej`
+
+let fromDecrypto = `
+Konto
+Akt
+Dostosowanie
+Reklama
+Umowa
+Powietrze
+Kwota
+Rozrywka
+Zwierzę
+Odpowiedź
+Aparat
+Zatwierdzenie
+Argument
+Sztuka
+Atak
+Próba
+Uwaga
+Przyciąganie
+Autorytet
+Powrót
+Równowaga
+Podstawa
+Zachowanie
+Wiara
+Urodzenie
+Bit
+Ugryzienie
+Krew
+Cios
+Ciało
+Mosiądz
+Chleb
+Oddech
+Brat
+Budynek
+Palić
+Pękać
+Biznes
+Masło
+Płótno
+Troska
+Przyczyna
+Kreda
+Szansa
+Zmiana
+Tkanina
+Węgiel
+Kolor
+Komfort
+Komitet
+Firma
+Porównanie
+Konkurencja
+Komputer
+Stan
+Połączenie
+Sterowanie
+Kucharz
+Miedź
+Kopia
+Korek
+Kaszel
+Kraj
+Pokrywa
+Pęknięcie
+Kredyt
+Przestępstwo
+Zmiażdżenie
+Płacz
+Prąd
+Krzywa
+Szkoda
+Niebezpieczeństwo
+Córka
+Dzień
+Śmierć
+Dług
+Decyzja
+Stopień
+Projektowanie
+Pragnienie
+Zniszczenie
+Szczegół
+Rozwój
+Trawienie
+Kierunek
+Odkrycie
+Dyskusja
+Choroba
+Obrzydzenie
+Odległość
+Dystrybucja
+Podział
+Zwątpienie
+Picie
+Jazda
+Pył
+Ziemia
+Krawędź
+Edukacja
+Efekt
+Koniec
+Błąd
+Wydarzenie
+Przykład
+Wymiana
+Istnienie
+Ekspansja
+Doświadczenie
+Ekspert
+Fakt
+Upadek
+Rodzina
+Ojciec
+Strach
+Uczucie
+Fikcja
+Pole
+Walka
+Ogień
+Płomień
+Lot
+Kwiat
+Fałda
+Jedzenie
+Siła
+Forma
+Przyjaciel
+Przód
+Owoce
+Szkło
+Złoto
+Rząd
+Ziarno
+Trawa
+Chwyt
+Internet
+Grupa
+Wzrost
+Przewodnik
+Port
+Harmonia
+Nienawiść
+Słuch
+Ciepło
+Pomoc
+Historia
+Dziura
+Nadzieja
+Godzina
+Humor
+Lód
+Pomysł
+Impuls
+Wzrost
+Przemysł
+Atrament
+Owad
+Instrument
+Ubezpieczenie
+Zainteresowanie
+Wynalazek
+Żelazo
+Galaretka
+Połączenie
+Podróż
+Sędzia
+Skok
+Kopnięcie
+Pocałunek
+Wiedza
+Ziemia
+Język
+Śmiech
+Niski
+Ołów
+Nauka
+Skóra
+Litera
+Poziom
+Winda
+Światło
+Limit
+Len
+Płyn
+Lista
+Wygląd
+Strata
+Miłość
+Maszyna
+Człowiek
+Menedżer
+Znak
+Rynek
+Masa
+Posiłek
+Miara
+Mięso
+Spotkanie
+Pamięć
+Metal
+Środek
+Mleko
+Umysł
+Kopalnia
+Minuta
+Mgła
+Pieniądze
+Miesiąc
+Ranek
+Matka
+Ruch
+Góra
+Ruch
+Muzyka
+Imię
+Naród
+Potrzeba
+Wiadomości
+Noc
+Hałas
+Nuta
+Liczba
+Obserwacja
+Oferta
+Ropa
+Operacja
+Opinia
+Zamówienie
+Organizacja
+Ozdoba
+Strona
+Ból
+Farba
+Papier
+Część
+Pasta
+Zapłata
+Pokój
+Osoba
+Miejsce
+Roślina
+Zabawa
+Przyjemność
+Punkt
+Trucizna
+Polska
+Portier
+Pozycja
+Proszek
+Władza
+Cena
+Druk
+Proces
+Produkcja
+Zysk
+Własność
+Proza
+Protest
+Pociągnięcie
+Kara
+Cel
+Pchnięcie
+Jakość
+Pytanie
+Deszcz
+Zasięg
+Tempo
+Promień
+Reakcja
+Odczyt
+Powód
+Zapis
+Żal
+Relacja
+Religia
+Prośba
+Szacunek
+Odpoczynek
+Nagroda
+Rytm
+Ryż
+Rzeka
+Droga
+Bułka
+Pokój
+Rub
+Zasada
+Bieg
+Sól
+Piasek
+Skala
+Nauka
+Morze
+Sekretarka
+Wybór
+Zmysł
+Sługa
+Cień
+Wstyd
+Szok
+Bok
+Znak
+Jedwab
+Srebro
+Siostra
+Rozmiar
+Niebo
+Sen
+Poślizg
+Zbocze
+Rozbić
+Zapach
+Uśmiech
+Dym
+Kichanie
+Śnieg
+Mydło
+Społeczeństwo
+Syn
+Piosenka
+Rodzaj
+Dźwięk
+Zupa
+Przestrzeń
+Scena
+Początek
+Para
+Stal
+Krok
+Szew
+Kamień
+Stop
+Historia
+Odcinek
+Struktura
+Substancja
+Cukier
+Sugestia
+Lato
+Wsparcie
+Niespodzianka
+Pływanie
+System
+Rozmowa
+Smak
+Podatek
+Nauczanie
+Tendencja
+Test
+Teoria
+Myśl
+Grzmot
+Czas
+Cyna
+Góra
+Dotyk
+Handel
+Transport
+Sztuczka
+Problem
+Obrót
+Skręt
+Jednostka
+Użycie
+Wartość
+Werset
+Statek
+Widok
+Głos
+Spacer
+Wojna
+Pranie
+Odpady
+Woda
+Fala
+Wosk
+Droga
+Pogoda
+Tydzień
+Waga
+Wiatr
+Wino
+Zima
+Kobieta
+Drewno
+Wełna
+Słowo
+Praca
+Rana
+Pismo
+Kąt
+Mrówka
+Jabłko
+Łuk
+Ramię
+Wojsko
+Dziecko
+Torba
+Piłka
+Taśma
+Dorzecze
+Kosz
+Wanna
+Łóżko
+Pszczoła
+Dzwonek
+Jagoda
+Ptak
+Ostrze
+Deska
+Łódź
+Kość
+Książka
+But
+Butelka
+Pudełko
+Chłopiec
+Mózg
+Hamulec
+Gałąź
+Cegła
+Most
+Szczotka
+Wiadro
+Żarówka
+Guzik
+Ciasto
+Aparat
+Karta
+Powóz
+Wóz
+Kot
+Łańcuch
+Ser
+Szachy
+Broda
+Kościół
+Koło
+Zegar
+Chmura
+Płaszcz
+Obroża
+Grzebień
+Sznurek
+Krowa
+Kubek
+Zasłona
+Poduszka
+Pies
+Drzwi
+Odpływ
+Szuflada
+Sukienka
+Kropla
+Ucho
+Jajko
+Silnik
+Oko
+Twarz
+Farma
+Pióro
+Palec
+Ryba
+Flaga
+Podłoga
+Mucha
+Stopa
+Widelec
+Ptactwo
+Rama
+Ogród
+Dziewczyna
+Rękawica
+Koza
+Broń
+Włosy
+Młotek
+Ręka
+Kapelusz
+Głowa
+Serce
+Hak
+Róg
+Koń
+Szpital
+Dom
+Wyspa
+Klejnot
+Czajnik
+Klucz
+Kolano
+Nóż
+Węzeł
+Liść
+Noga
+Biblioteka
+Linia
+Warga
+Zamek
+Mapa
+Mecz
+Małpa
+Księżyc
+Usta
+Mięsień
+Paznokieć
+Szyja
+Igła
+Nerw
+Sieć
+Nos
+Orzech
+Biuro
+Pomarańcza
+Piekarnik
+Paczka
+Długopis
+Ołówek
+Zdjęcie
+Świnia
+Szpilka
+Rura
+Samolot
+Talerz
+Pług
+Kieszeń
+Garnek
+Ziemniak
+Więzienie
+Pompa
+Szyna
+Szczur
+Paragon
+Pierścień
+Pręt
+Dach
+Korzeń
+Żagiel
+Szkoła
+Nożyczki
+Śruba
+Nasiona
+Owce
+Półka
+Statek
+Koszula
+But
+Skóra
+Spódnica
+Wąż
+Skarpeta
+Szpadel
+Gąbka
+Łyżka
+Wiosna
+Kwadrat
+Znaczek
+Gwiazda
+Stacja
+Łodyga
+Kij
+Pończocha
+Brzuch
+Sklep
+Ulica
+Słońce
+Stół
+Ogon
+Nić
+Gardło
+Kciuk
+Bilet
+Palec
+Język
+Ząb
+Miasto
+Pociąg
+Taca
+Drzewo
+Spodnie
+Parasol
+Ściana
+Zegarek
+Koło
+Bicz
+Gwizdek
+Okno
+Skrzydło
+Drut
+Robak
+Walka
+Lot
+Trucizna
+Ziemia
+Kamień
+Robak
+Duch
+Stal
+Ogień
+Woda
+Trawa
+Elektryczność
+Psychika
+Lód
+Smok
+Mrok
+Baśń
+`
